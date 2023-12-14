789e65f57a13c594203510c25ff034f7f9e9a49c
Worrasin Damrongtaveesak <105853885+Worrasin@users.noreply.github.com>
2023-12-15 09:02:12 +0930

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,7 +185,7 @@
   }
 
   // Function to handle image loading errors
-  function handleImageError(img) {
+  window.handleImageError(img) {
     // img.onerror = null; // Prevent infinite loop if "no-image.jpg" is also not found
     img.src = "./assets/img/menu/no-image.jpg"; // Replace with the path to your "no-image.jpg" file
   };
